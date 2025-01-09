e8274db65499ef70e059be661f0b3c5361a5edb6
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-09 11:48:09 +0300

    Update Program.cs

--- Practical - Library/Program.cs
+++ Practical - Library/Program.cs
@@ -2,6 +2,6 @@
 
 
 using PatikaLibrary;
-
+//new anahtar kelimesi, Books sınıfından bir nesne oluşturmak için kullanılır.
 Books books = new Books();
 Books books2 = new Books("1984", "George", "Orwell", "328", "Penguin");
