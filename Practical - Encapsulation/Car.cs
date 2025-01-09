5066be9dbfcaaad531817dc1379cd46db685ebac
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-09 12:09:03 +0300

    Encapsulation Practical
    Encapsulation Practical Car Project

---
+++ Practical - Encapsulation/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalEncapsulation
+{
+    public class Car
+    {
+        public string Brand {  get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        private int doorCount;
+        //Kapı sayısı 2 veya 4 olabilir. Kapsülleme yaparak bu kuralı sağlayalım.
+        public int DoorCount
+        {
+            get
+            {
+                return doorCount;
+            }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    doorCount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Kapi sayisi 2 veya 4 olmalidir.");
+                    doorCount -= 1;
+                }
+            }
+        }
+
+     
+        public Car(string brand, string model, string color, int doorCount)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;
+            DoorCount = doorCount;
+            Console.WriteLine($"Araba olusturuldu.\nMarka : {Brand} , \nModel: {Model}, \nRenk: {Color}, \nKapı Sayisi: {DoorCount} ");
+        }
+
+
+
+    }
+}
---
+++ Practical - Encapsulation/Program.cs
@@ -0,0 +1,5 @@
+﻿using PracticalEncapsulation;
+
+Car car = new Car("Ford", "Focus", "Black", 5);
+
+Car car1 = new Car("Toyota", "Corolla", "White", 4);
