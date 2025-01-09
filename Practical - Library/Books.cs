8516adb560161e67a311deb03ac3dada34eacb1b
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-09 11:47:54 +0300

    Update Books.cs

--- Practical - Library/Books.cs
+++ Practical - Library/Books.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 namespace PatikaLibrary
 {
+       //Kitap sınıfının propertyleri -> Kitap Adı , Yazar Adı , Yazar Soyadı , Sayfa Sayısı , Yayıncı , Yayın Tarihi
     public class Books
     {
+        //Propertyler private olarak tanımlandı.
+//Propertylerin dışarıdan erişilebilmesi için get ve set metodları yazıldı.
         private string BookName { get; set; }
         private string AuthorName { get; set; }
         private string AuthorSurname { get; set; }
         private int NumberofPages { get; set; }
         private string Publisher { get; set; }
         private DateTime PublishDate { get; set; }
-
+//1.si Default Constructor tarzında parametre almayan bir metot.
         public Books()
         {
             BookName = "";
@@ -25,7 +28,7 @@ namespace PatikaLibrary
             PublishDate = DateTime.Today;
             Console.WriteLine($"Kayit tarihi: {PublishDate}");
         }
-
+    //2.si Ad, Soyad, Sayfa Sayısı ve Yayıncı parametrelerini alarak newleme işlemi sırasında değerleri propertylere atayan alternatif constructor.
         public Books(string bookName, string authorName, string authorSurname, int numberofPages, string publisher)
         {
             BookName = bookName;
@@ -37,7 +40,8 @@ namespace PatikaLibrary
             Console.WriteLine($"Kitap Adı : {bookName} , Yazar Adı,Soyadı {authorName},{authorSurname},Sayfa sayisi: {numberofPages},Yayıncı: {publisher}");
             Console.WriteLine($"Kayit tarihi: {PublishDate}");
 
-
+      //Default Constructor: Parametresiz bir şekilde bir kitap nesnesi oluşturur ve varsayılan değerleri atar.
+ // Parameterized Constructor: Kitapla ilgili bilgileri parametre olarak alır ve nesne oluşturma aşamasında bu bilgileri atar.
         }
     }
 }
