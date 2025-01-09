937d3e6b388dd86e533d500e963d7ec43f0db2d5
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-09 10:37:36 +0300

    Add files via upload

---
+++ Practical - Library/Books.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaLibrary
+{
+    public class Books
+    {
+        private string BookName { get; set; }
+        private string AuthorName { get; set; }
+        private string AuthorSurname { get; set; }
+        private int NumberofPages { get; set; }
+        private string Publisher { get; set; }
+        private DateTime PublishDate { get; set; }
+
+        public Books()
+        {
+            BookName = "";
+            AuthorName = "";
+            AuthorSurname = "";
+            NumberofPages = 0;
+            Publisher = "";
+            PublishDate = DateTime.Today;
+            Console.WriteLine($"Kayit tarihi: {PublishDate}");
+        }
+
+        public Books(string bookName, string authorName, string authorSurname, int numberofPages, string publisher)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+            AuthorSurname = authorSurname;
+            NumberofPages = numberofPages;
+            Publisher = publisher;
+            PublishDate = DateTime.Today;
+            Console.WriteLine($"Kitap Adı : {bookName} , Yazar Adı,Soyadı {authorName},{authorSurname},Sayfa sayisi: {numberofPages},Yayıncı: {publisher}");
+            Console.WriteLine($"Kayit tarihi: {PublishDate}");
+
+
+        }
+    }
+}
---
+++ Practical - Library/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+
+using PatikaLibrary;
+
+Books books = new Books();
+Books books2 = new Books("1984", "George", "Orwell", "328", "Penguin");
