ce927524fbc33d4fb1d8b3147802a9671e008d95
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-16 22:35:17 +0300

    Practical List 1

---
+++ Practical - List1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Patika Plus Gala Gecesine Hoşgeldiniz.");
+
+List<string> patikaGalaList = new List<string>()
+{
+    "Bulent Ersoy",
+    "Ajda Pekkan",
+    "Ebru Gundes",
+    "Hadise",
+    "Hande Yener",
+    "Tarkan",
+    "Funda Arar",
+    "Demet Akalın"
+};
+Console.WriteLine("** Davetliler **");
+
+int sıra = 1;//sıra numarası 
+
+foreach (var item in patikaGalaList)
+{
+    Console.WriteLine($"{sıra} - {item}");
+    sıra++;
+}
