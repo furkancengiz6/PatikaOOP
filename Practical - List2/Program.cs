4079d2656440fa78592f3a9b80c4c1101109325a
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-16 22:57:35 +0300

    Add files via upload

---
+++ Practical - List2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+List<string> cafeList = new List<string>();
+
+for (int i = 1; i <6; i++)
+{
+    Console.Write($"Kahve {i} :");
+    string input  = Console.ReadLine();
+    cafeList.Add(input);
+}
+Console.WriteLine("Girilen Kahve İsimleri:");
+foreach ( string cafe in cafeList) {  Console.WriteLine( cafe ); }
