f79087f410546fbb2735f7401697619e8a93518e
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-19 20:30:26 +0300

    Add files via upload

---
+++ Pratical - TryCatch/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz:");
+string input = Console.ReadLine();
+try
+{
+    
+    double number = double.Parse(input);
+    if (number < 0)
+    {
+        Console.WriteLine("Negatif hatalı islem");
+    }
+    else
+    {
+
+        Console.WriteLine($"Karekok : {Math.Sqrt(number)}");
+
+    } 
+}
+catch (FormatException)
+{
+    
+    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+}
